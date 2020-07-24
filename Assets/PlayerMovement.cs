6a2bf81897e0bd55fdd31ca6612e68bb3546edb3
Michal Waszczuk <ihoold@gmail.com>
2020-07-24 18:50:21 +0200

    Movement base

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 30;
+    Rigidbody2D body => GetComponent<Rigidbody2D>();
+    Vector2 movement;
+
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate() 
+    {
+        body.MovePosition(body.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
