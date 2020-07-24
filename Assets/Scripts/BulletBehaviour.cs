865a647268d674d2fb14eb53a6320425b5f34b7b
Michal Waszczuk <ihoold@gmail.com>
2020-07-24 22:08:26 +0200

    Bullet gravity

--- Assets/Scripts/BulletBehaviour.cs
+++ Assets/Scripts/BulletBehaviour.cs
@@ -1,11 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BulletBehaviour : MonoBehaviour
 {
+    public float attraction = 50f;
+    Rigidbody2D body => GetComponent<Rigidbody2D>();
+
+
     void OnCollisionEnter2D(Collision2D col) {
         // Special effects, damaging enemies
+        if (col.gameObject.tag == "Enemy") {
+            Debug.Log("Hit!");
+        }
+
         Destroy(this.gameObject);
+    }
+
+
+
+    void OnTriggerStay2D(Collider2D other) {
+        if (other.tag == "EnemyGravity") {
+            Vector2 direction = other.GetComponentInParent<Rigidbody2D>().position - body.position;
+            body.AddForce(direction * attraction / (direction.magnitude * direction.magnitude));
+        } 
     }
 }
