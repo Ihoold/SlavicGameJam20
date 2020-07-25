5450da4143d89222d66fc576671fe63e2ce39e40
Michal Waszczuk <ihoold@gmail.com>
2020-07-25 16:51:47 +0200

    Use audio mixer

--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -1,14 +1,29 @@
 ﻿using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Options : MonoBehaviour
 {
+    public AudioMixer audio;
+    public Slider slider; 
+
+    public void Start() {
+        float vol;
+        audio.GetFloat("Volume", out vol);
+        slider.value = vol;
+    }
+
     public void Back() {
         Debug.Log("LoadScene");
         SceneManager.LoadScene("Menu");
     }
 
-    public void Update() {
+    public void SetVolume(float volume) {
+        audio.SetFloat("Volume", volume);
+    }
+
+    void Update() {
         if (Input.GetButtonDown("Cancel")) {
             Back();
         }
